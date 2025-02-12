637152c0241de417563a09b9c4ec38fefcf44b64
HenriqueCCicero <henrique.c.cicero@hotmail.com>
2025-02-12 20:16:19 -0300

    criar arquivo view show para mostrar usuario especifico

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Mostrar Usuario {{ $user->name }}</h1>
+    {{ dd($user) }}
+</body>
+</html>
